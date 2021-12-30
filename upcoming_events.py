f08e86149a35013b1457c10c1a12be2289b0bea1
Nickich <pravoslavie1488@gmail.com>
2021-12-30 04:22:17 +0300

    Added stdout in upcomings_events.py

--- upcoming_events.py
+++ upcoming_events.py
@@ -1,9 +1,12 @@
 import re
 import requests
+import sys
 
 link = 'https://www.python.org'
 upcoming_events_pattern = r'<a href="/events/.+/\d+/">(.+)</a></li>'
 
 python_org_html = requests.get(link)
 upcoming_events = re.findall(upcoming_events_pattern, python_org_html.text)
-print(upcoming_events)
+stdout_file = sys.stdout
+for event in upcoming_events:
+    stdout_file.write(event + '\n')
